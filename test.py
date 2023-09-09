5b1b3214067f4f8b92c11beb71655bd6ada96294
Cashaqu <131269265+Cashaqu@users.noreply.github.com>
2023-09-09 23:28:48 +0300

    deleting garbage

--- test.py
+++
@@ -1,6 +0,0 @@
-from preprocessing import removing_stopword, removing_punctuation, tokenization, removing_short_tokens, creating_lemmas, cleaning_text
-from summarization import get_summary
-from simularity import extract_simular_wine
-
-
-extract_simular_wine([4,8,15,16,23])
